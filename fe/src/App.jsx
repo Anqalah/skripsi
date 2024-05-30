e3c7106000552582bf571e16d2f7d019159d3a1c
Anqalah <evodark7@gmail.com>
2024-05-30 12:06:12 +0800

    SETUP FE

---
+++ fe/src/App.jsx
@@ -0,0 +1,11 @@
+import './app.css'
+function App() {
+  return (
+    <div className="App">
+      <div className="rgb(255, 0, 0) flex justify-center bg-slate-600 min-h-screen items-center">
+        SKRIPSI CUY
+      </div>
+    </div>
+  );
+}
+export default App;
