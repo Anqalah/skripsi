3c63694faceac6b6bd9d352f91517321954675a1
Anqalah <evodark7@gmail.com>
2024-06-05 23:39:30 +0800

    Implementasi atomic desain

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,11 +1,16 @@
-import './app.css'
+import React from "react";
+import Button from "./components/Elements/Button";
+
 function App() {
   return (
-    <div className="App">
-      <div className="rgb(255, 0, 0) flex justify-center bg-slate-600 min-h-screen items-center">
-        SKRIPSI CUY
+    <div className="flex justify-center bg-slate-600 min-h-screen items-center">
+      <div className="flex gap-x-3">
+        <Button variant="bg-blue-700">Login</Button>
+        <Button variant="bg-red-700">Logout</Button>
+        <Button variant="bg-black">Register</Button>
       </div>
     </div>
   );
 }
+
 export default App;
