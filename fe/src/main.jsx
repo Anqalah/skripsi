bc4defb2c4395097a6a526d4540ef623763b1cb9
Anqalah <evodark7@gmail.com>
2024-06-06 20:17:15 +0800

    Testing React Deploy Vercel

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -5,13 +5,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./Pages/login";
 import RegisterPage from "./Pages/register";
 import ErrorPage from "./Pages/error";
+import { Link } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <h1 className="flex font-bold text-[#8d99ae] bg-[#2b2d42] justify-center text-[86px] min-h-screen items-center">
-        SKRIPSI LEE
+        <Link to="/register" className="font-bold text-[#457b9d]">
+          SKRIPSI LEE
+        </Link>
       </h1>
     ),
     errorElement: <ErrorPage />,
