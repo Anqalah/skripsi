c8541486747fd2a4590d3647b574be216d7c1021
Anqalah <evodark7@gmail.com>
2024-06-06 14:05:30 +0800

    Implementasi InputForm

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import Button from "./components/Elements/Button";
+import InputForm from "./components/Elements/Input";
 
 function App() {
   return (
-    <div className="flex justify-center bg-slate-600 min-h-screen items-center">
-      <div className="flex gap-x-3">
-        <Button variant="bg-blue-700">Login</Button>
-        <Button variant="bg-red-700">Logout</Button>
-        <Button variant="bg-black">Register</Button>
+    <div className="flex justify-center min-h-screen items-center">
+      <div className="w-full max-w-xs">
+        <h1 className="text-3xl font-bold md-2 text-blue-600"></h1>
+        <p className="font-medium text-slate-500 mb-8">
+          Selamat Datang, Silahkan masukkan akun anda
+        </p>
+        <form action="">
+          <InputForm
+            label="email"
+            type="email"
+            placeholder="contoh@gmail.com"
+            name="email"
+          />
+          <InputForm
+            label="password"
+            type="password"
+            placeholder="******"
+            name="password"
+          />
+        </form>
       </div>
     </div>
   );
