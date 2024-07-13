cb02dcb10277ba2b65e7da347da43d86e78b55f8
anqalah <evodark7@gmail.com>
2024-07-13 20:37:08 +0800

    Setup Model

---
+++ be/models/ProductModel.js
@@ -0,0 +1,38 @@
+import { Sequelize } from "sequelize";
+import db from "../config/database";
+import Users from "./UserModel";
+
+const { DataTypes } = Sequelize;
+const Products = db.define(
+  "product",
+  {
+    uuid: {
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  },
+  { freezeTableName: true }
+);
+
+Users.hasMany(Products);
+Products.belongsTo(Users);
+
+export default Products;
