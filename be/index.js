e9d25ac6011655b2492c1280d8868ce32f0a548b
anqalah <evodark7@gmail.com>
2024-07-17 23:33:36 +0800

    Session Add

--- be/index.js
+++ be/index.js
@@ -2,22 +2,30 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 import db from "./config/Database.js";
+import SequelizeStore from "connect-session-sequelize";
 import dotenv from "dotenv";
-import userRoute from "./routes/UserRoute.js";
-import productRoute from "./routes/ProductRoute.js";
+import UserRoute from "./routes/UserRoute.js";
+import ProductRoute from "./routes/ProductRoute.js";
+import AuthRoute from "./routes/AuthRoute.js";
 dotenv.config();
 
 const app = express();
 
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({ db: db });
 // (async () => {
 //   await db.sync();
 // })();
+
+// store.sync();
 
 app.use(
   session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized: true,
+    store: store,
     secure: "auto",
   })
 );
@@ -29,8 +37,9 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(userRoute);
-app.use(productRoute);
+app.use(UserRoute);
+app.use(ProductRoute);
+app.use(AuthRoute);
 
 app.listen(process.env.APP_PORT, () => {
   console.log("Server Sedang Berjalan...");
